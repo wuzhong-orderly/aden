f140845cd00e00b37ac1ad7cf6aba9584b0e9ad7
Jame Zhan <jame@orderly.network>
2024-12-12 12:22:35 +0800

    update TradingPage import

--- app/routes/perp.$symbol.tsx
+++ app/routes/perp.$symbol.tsx
@@ -1,18 +1,11 @@
-import { lazy, Suspense, useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "@remix-run/react";
 import { MetaFunction } from "@remix-run/node";
 import { API } from "@orderly.network/types";
-import { Flex, Spinner } from "@orderly.network/ui";
-// import { TradingPage } from "@orderly.network/trading";
+import { TradingPage } from "@orderly.network/trading";
 import config from "@/utils/config";
 import { updateSymbol } from "@/utils/storage";
 import { formatSymbol, generatePageTitle } from "@/utils/utils";
-
-const TradingPage = lazy(() =>
-  import("@orderly.network/trading").then((module) => ({
-    default: module.TradingPage,
-  }))
-);
 
 export const meta: MetaFunction = ({ params }) => {
   return [{ title: generatePageTitle(formatSymbol(params.symbol!)) }];
@@ -34,23 +27,6 @@ export default function PerpPage() {
       navigate(`/perp/${symbol}`);
     },
     [navigate]
-  );
-
-  return (
-    <Suspense
-      fallback={
-        <Flex style={{ height: "100vh" }} justify="center" itemAlign="center">
-          <Spinner />
-        </Flex>
-      }
-    >
-      <TradingPage
-        symbol={symbol}
-        onSymbolChange={onSymbolChange}
-        tradingViewConfig={config.tradingPage.tradingViewConfig}
-        sharePnLConfig={config.tradingPage.sharePnLConfig}
-      />
-    </Suspense>
   );
 
   return (
