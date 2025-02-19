e68922aa701decbe597dbbac2334814f4c218bfe
Mario Reder <mario.reder@pm.me>
2025-02-19 10:16:39 +0100

    update sdk and add monad testnet

--- app/components/orderlyProvider/index.tsx
+++ app/components/orderlyProvider/index.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from "react";
 import { WalletConnectorProvider } from "@orderly.network/wallet-connector";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
 import config from "@/utils/config";
+import { ArbitrumSepolia, SolanaDevnet } from "@orderly.network/types";
 
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
   return (
@@ -10,7 +11,17 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
         brokerId={import.meta.env.VITE_ORDERLY_BROKER_ID}
         brokerName={import.meta.env.VITE_ORDERLY_BROKER_NAME}
         networkId={import.meta.env.VITE_NETWORK_ID}
-        chainFilter={import.meta.env.VITE_NETWORK_ID === 'mainnet' ? {testnet: []} : {mainnet: []}}
+        chainFilter={import.meta.env.VITE_NETWORK_ID === 'mainnet' ? {testnet: []} : {mainnet: [], testnet: [ArbitrumSepolia, SolanaDevnet, {
+          id: 10143,
+          chainInfo: {
+            name: "Monad Testnet",
+            public_rpc_url: "https://testnet-rpc.monad.xyz",
+            chainId: 10143,
+            currency_symbol: "MON",
+            explorer_base_url: "",
+            vault_address: "",
+          },
+        }]}}
         appIcons={config.orderlyAppProvider.appIcons}
       >
         {props.children}
