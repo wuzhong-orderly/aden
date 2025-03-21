298215f0c5ceab7138be574190c3534022b9a5a6
Mario Reder <mario.reder@pm.me>
2025-03-21 22:47:53 +0100

    feat: wallet connect

--- app/components/orderlyProvider/index.tsx
+++ app/components/orderlyProvider/index.tsx
@@ -4,6 +4,9 @@ import { OrderlyAppProvider } from "@orderly.network/react-app";
 import config from "@/utils/config";
 import { NetworkId } from "@orderly.network/types";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import injected from "@web3-onboard/injected-wallets";
+import walletConnect from '@web3-onboard/walletconnect'
+import binance from "@binance/w3w-blocknative-connector";
 
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
 	const networkId = import.meta.env.VITE_NETWORK_ID as NetworkId;
@@ -23,7 +26,24 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
   );
   
   return (
-    <WalletConnectorProvider solanaInitial={{network: networkId === 'mainnet' ? WalletAdapterNetwork.Mainnet : WalletAdapterNetwork.Devnet}}>
+    <WalletConnectorProvider
+      solanaInitial={{network: networkId === 'mainnet' ? WalletAdapterNetwork.Mainnet : WalletAdapterNetwork.Devnet}}
+      evmInitial={import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID && typeof window !== 'undefined' ? {
+        options: {
+          wallets: [
+            injected(),
+            binance({ options: { lng: "en" } }),
+            walletConnect({
+              projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+              qrModalOptions: {
+                themeMode: "dark",
+              },
+              dappUrl: window.location.origin,
+            }),
+          ],
+        }
+      } : undefined}
+    >
       <OrderlyAppProvider
         brokerId={import.meta.env.VITE_ORDERLY_BROKER_ID}
         brokerName={import.meta.env.VITE_ORDERLY_BROKER_NAME}
