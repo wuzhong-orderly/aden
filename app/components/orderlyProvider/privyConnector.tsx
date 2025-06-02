78c202def8ecf00ed822c260f0e82c587513c5e4
Mario Reder <mario.reder@pm.me>
2025-06-02 11:28:29 +0200

    abstract wallet config

--- app/components/orderlyProvider/privyConnector.tsx
+++ app/components/orderlyProvider/privyConnector.tsx
@@ -6,6 +6,7 @@ import { CreateConnectorFn } from 'wagmi';
 import { Adapter, WalletError , WalletAdapterNetwork, WalletNotReadyError } from '@solana/wallet-adapter-base';
 import { LedgerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { createDefaultAddressSelector, createDefaultAuthorizationResultCache, SolanaMobileWalletAdapter } from '@solana-mobile/wallet-adapter-mobile';
+import { QueryClient } from "@tanstack/query-core";
 
 const PrivyConnector = ({ children, networkId }: {
   children: ReactNode;
@@ -14,6 +15,7 @@ const PrivyConnector = ({ children, networkId }: {
   const appId = import.meta.env.VITE_PRIVY_APP_ID;
   const termsOfUseUrl = import.meta.env.VITE_PRIVY_TERMS_OF_USE;
   const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
+  const enableAbstractWallet = import.meta.env.VITE_ENABLE_ABSTRACT_WALLET === 'true';
   const isBrowser = typeof window !== 'undefined';
 
   const connectors: CreateConnectorFn[] = [injected()];
@@ -69,6 +71,9 @@ const PrivyConnector = ({ children, networkId }: {
         },
         appid: appId,
       }}
+      abstractConfig={enableAbstractWallet ? {
+        queryClient: new QueryClient(),
+      } : undefined}
     >
       {children}
     </WalletConnectorPrivyProvider>
