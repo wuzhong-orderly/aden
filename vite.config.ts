243980fd25a225b39b23fc10bcd694ae64d8eaab
Wuzhong <wuzhong@orderly.network>
2025-07-17 02:50:42 +0800

    Revert "Remove V2 path since it is causing many problem"
    This reverts commit 8c4e711491a08b0e5d1e9c9580b5d5966d24608c.

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(() => {
     noExternal.push("ethers");
   }
 
-  const basePath = process.env.PUBLIC_PATH || "/";
+  const basePath = process.env.PUBLIC_PATH || "/v2/";
 
   return {
     base: basePath,
